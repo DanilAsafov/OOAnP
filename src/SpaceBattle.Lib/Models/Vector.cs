4df066e26f8a014d2c5b65ecacb59f73b284f2d7
DanilAsafov <perez3498145@gmail.com>
2026-05-21 18:07:51 +0000

    feat: implement Vector model with n-dimensional support

---
+++ src/SpaceBattle.Lib/Models/Vector.cs
@@ -0,0 +1,40 @@
+﻿namespace SpaceBattle.Lib.Models;
+
+public class Vector(int[] coord)
+{
+    private readonly int[] coordinates = coord ?? throw new ArgumentNullException(nameof(coord));
+
+    public int Dimension => coordinates.Length;
+
+    public override bool Equals(object? obj) => obj is Vector vector && coordinates.SequenceEqual(vector.coordinates);
+    
+
+    public override int GetHashCode()
+    {
+        var hash = new HashCode();
+        foreach (var c in coordinates)
+        {
+            hash.Add(c);
+        }
+
+        return hash.ToHashCode();
+    }
+
+    public static Vector operator +(Vector? vector1, Vector? vector2)
+    {
+        ArgumentNullException.ThrowIfNull(vector1);
+        ArgumentNullException.ThrowIfNull(vector2);
+
+        if (vector1.Dimension != vector2.Dimension) throw new ArgumentException("Размерности векторов должны совпадать.");
+
+        return new Vector([.. vector1.coordinates.Zip(vector2.coordinates, (v1, v2) => v1 + v2)]);
+    }
+
+    public static bool operator ==(Vector? vector1, Vector? vector2)
+    {
+        if (vector1 is null) return vector2 is null;
+        return vector1.Equals(vector2);
+    }
+
+    public static bool operator !=(Vector? vector1, Vector? vector2) => !(vector1 == vector2);
+}
---
+++ tests/SpaceBattle.Lib.Tests/Models/VectorTest.cs
@@ -0,0 +1,74 @@
+﻿namespace SpaceBattle.Lib.Tests.Models;
+
+public class VectorTest
+{
+    [Fact]
+    public void Constructor_NullArray_ThrowsArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(() => new Vector(null!));
+    }
+
+    [Fact]
+    public void OperatorPlus_ValidVectors_ReturnsCorrectSum()
+    {
+        var vector1 = new Vector([1, -1, 2]);
+        var vector2 = new Vector([-1, 1, -2]);
+        var expected = new Vector([0, 0, 0]);
+
+        var result = vector1 + vector2;
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void OperatorPlus_DifferentDimensions_ThrowsArgumentException()
+    {
+        var vector1 = new Vector([1, 2, 3]);
+        var vector2 = new Vector([1, 2]);
+
+        Assert.Throws<ArgumentException>(() => vector1 + vector2);
+    }
+
+    [Fact]
+    public void Equals_SameCoordinates_ReturnsTrue()
+    {
+        var vector1 = new Vector([5, -10]);
+        var vector2 = new Vector([5, -10]);
+
+        Assert.True(vector1.Equals(vector2));
+    }
+
+    [Fact]
+    public void Equals_DifferentType_ReturnsFalse()
+    {
+        var vector = new Vector([0]);
+        Assert.False(vector.Equals("String"));
+    }
+
+    [Fact]
+    public void OperatorEquals_BothNull_ReturnsTrue()
+    {
+        Vector? vector1 = null;
+        Vector? vector2 = null;
+
+        Assert.True(vector1 == vector2);
+    }
+
+    [Fact]
+    public void OperatorNotEquals_DifferentCoordinates_ReturnsTrue()
+    {
+        var vector1 = new Vector([1, -2]);
+        var vector2 = new Vector([-1, 2]);
+
+        Assert.True(vector1 != vector2);
+    }
+
+    [Fact]
+    public void GetHashCode_SameCoordinates_ReturnsSameHash()
+    {
+        var vector1 = new Vector([1, -2]);
+        var vector2 = new Vector([1, -2]);
+
+        Assert.Equal(vector1.GetHashCode(), vector2.GetHashCode());
+    }
+}
